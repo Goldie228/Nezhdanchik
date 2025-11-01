885d05c622e7f8f797a00853515a51ef9d0efbd1
Goldie228 <glebkazhuro@gmail.com>
2025-11-01 12:24:38 +0300

    chore(dish-show): minor fixes and UI improvements

--- app/helpers/dish_helper.rb
+++ app/helpers/dish_helper.rb
@@ -13,4 +13,21 @@ module DishHelper
     parts << "#{mins} мин" if mins.positive?
     parts.join(" ")
   end
+
+  def ingredients_count_badge(count)
+    word = russian_pluralize(count, "ингредиент", "ингредиента", "ингредиентов")
+    content_tag(:span, "#{count} #{word}", class: "badge badge-primary badge-md md:badge-lg")
+  end
+
+  private
+
+  def russian_pluralize(number, one, few, many)
+    return many if (11..14).include?(number % 100)
+
+    case number % 10
+    when 1 then one
+    when 2..4 then few
+    else many
+    end
+  end
 end
