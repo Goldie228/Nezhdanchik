84a6bdb029390f29e7b645f983e6444a48f30ade
Goldie228 <glebkazhuro@gmail.com>
2025-11-15 10:25:49 +0300

    feat(layout): arrange tables and seats properly

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,64 +1,120 @@
 class ReservationsController < ApplicationController
-  # В реальном приложении здесь будет аутентификация, например, через before_action :authenticate_user!
+  before_action :authenticate_user!
+
+  STATUSES = { 
+    available: "available", 
+    reserved: "reserved", 
+    occupied: "occupied"
+  }.freeze
+
+  STATUS_CLASSES = {
+    available: "bg-success",
+    reserved:  "bg-warning",
+    occupied:  "bg-error"
+  }.freeze
+
+  SEATS_COORDS = [
+    { num: 1,  x_percent: 37,   y_percent: 29   },
+    { num: 2,  x_percent: 46.4, y_percent: 29   },
+    { num: 3,  x_percent: 37,   y_percent: 47.5 },
+    { num: 4,  x_percent: 46.4, y_percent: 47.5 },
+    { num: 5,  x_percent: 47,   y_percent: 8    },
+    { num: 6,  x_percent: 51,   y_percent: 17.8 },
+    { num: 7,  x_percent: 59.8, y_percent: 17.8 },
+    { num: 8,  x_percent: 68.8, y_percent: 17.8 },
+    { num: 9,  x_percent: 78,   y_percent: 17.8 },
+    { num: 10, x_percent: 81.1, y_percent: 7.9  },
+    { num: 11, x_percent: 81,   y_percent: 29.5 },
+    { num: 12, x_percent: 90.5, y_percent: 29.5 },
+    { num: 13, x_percent: 81,   y_percent: 48   },
+    { num: 14, x_percent: 90.5, y_percent: 48   },
+    { num: 15, x_percent: 81,   y_percent: 69.0 },
+    { num: 16, x_percent: 75.0, y_percent: 59.0 },
+    { num: 17, x_percent: 65.9, y_percent: 59.0 },
+    { num: 18, x_percent: 57.8, y_percent: 59.0 },
+    { num: 19, x_percent: 50.0, y_percent: 59.0 },
+    { num: 20, x_percent: 46.3, y_percent: 69.0 }
+  ]
+
+  TABLE_COORDS = [
+    { num: 1, x_percent: 38.5, y_percent: 38.5, width_percent: 10, height_percent: 5 },
+    { num: 2, x_percent: 54.5, y_percent: 7.5,  width_percent: 21, height_percent: 5 },
+    { num: 3, x_percent: 82.5, y_percent: 38.5, width_percent: 10, height_percent: 5 },
+    { num: 4, x_percent: 54,   y_percent: 68.5, width_percent: 21, height_percent: 5 }
+  ]
 
   def show
-    # --- Моковые данные для столиков ---
-    @tables = [
-      { id: 1,  x_percent: 37, y_percent: 29, status: "available" },
-      { id: 2,  x_percent: 46.4, y_percent: 29, status: "reserved" },
-      { id: 3,  x_percent: 37, y_percent: 47.5, status: "occupied" },
-      { id: 4,  x_percent: 46.4, y_percent: 47.5, status: "available" },
-      { id: 5,  x_percent: 47, y_percent: 8, status: "available" },
-      { id: 6,  x_percent: 51, y_percent: 17.8, status: "reserved" },
-      { id: 7,  x_percent: 59.8, y_percent: 17.8, status: "available" },
-      { id: 8,  x_percent: 68.8, y_percent: 17.8, status: "occupied" },
-      { id: 9,  x_percent: 78, y_percent: 17.8, status: "available" },
-      { id: 10, x_percent: 81.1, y_percent: 7.9, status: "available" },
-      { id: 11, x_percent: 81, y_percent: 29.5, status: "available" },
-      { id: 12, x_percent: 90.5, y_percent: 29.5, status: "reserved" },
-      { id: 13, x_percent: 81, y_percent: 48, status: "available" },
-      { id: 14, x_percent: 90.5, y_percent: 48, status: "occupied" },
-      { id: 15, x_percent: 81, y_percent: 69.0, status: "available" },
-      { id: 16, x_percent: 75.0, y_percent: 59.0, status: "available" },
-      { id: 17, x_percent: 65.9, y_percent: 59.0, status: "available" },
-      { id: 18, x_percent: 57.8, y_percent: 59.0, status: "available" },
-      { id: 19, x_percent: 50.0, y_percent: 59.0, status: "reserved" },
-      { id: 20, x_percent: 46.3, y_percent: 69.0, status: "available" }
-    ]
+    set_seats
+    set_working_hours
+    set_cart
+    set_tables
+  end
 
+  private
+
+  def set_seats
+    seats = Seat.all
+
+    @seats = SEATS_COORDS.map do |coords|
+      seat = seats.find_by(number: coords[:num])
+      next unless seat
+
+      {
+        id:        seat.number,
+        x_percent: coords[:x_percent],
+        y_percent: coords[:y_percent],
+        status:    seat.active ? STATUSES[:available] : STATUSES[:occupied],
+        table_id:  seat.table_id
+      }
+    end.compact
+  end
+
+  def set_working_hours
     @working_hours = {
       1..4 => { open: "09:00", close: "23:00" }, # Пн-Чт
       5..6 => { open: "09:00", close: "00:00" }, # Пт-Сб
       0..0 => { open: "09:00", close: "23:00" }  # Вс
     }
+  end
 
+  def working_hours_for(day)
+    case day
+    when 1..4 then { open: "09:00", close: "23:00" }
+    when 5..6 then { open: "09:00", close: "00:00" }
+    else           { open: "09:00", close: "23:00" }
+    end
+  end
+
+  def set_cart
     @cart = Cart.for_user!(current_user)
 
     @total_price = @cart.total_cents / 100.0
     @total_items = @cart.total_items_count
   end
 
-  # В реальном приложении здесь будет действие create для обработки формы
-  # def create
-  #   # Логика сохранения брони в базу данных
-  #   # ...
-  #   redirect_to root_path, notice: 'Ваша бронь успешно оформлена!'
-  # end
+  def set_tables
+    tables = Table.order(:id)
 
-  private
+    @tables = TABLE_COORDS.zip(tables).map do |coords, table|
+      next unless table
 
-  def status_class(status)
-    case status.to_sym
-    when :available
-      "bg-success"
-    when :reserved
-      "bg-warning"
-    when :occupied
-      "bg-error"
-    else
-      "bg-base-300"
-    end
+      {
+        id:             table.id,
+        name:           table.name,
+        status:         table.active ? STATUSES[:available] : STATUSES[:occupied],
+        seats_count:    table.seats_count,
+        booking_price:  table.booking_price,
+        x_percent:      coords[:x_percent],
+        y_percent:      coords[:y_percent],
+        width_percent:  coords[:width_percent],
+        height_percent: coords[:height_percent]
+      }
+    end.compact
   end
 
-  helper_method :status_class
+  def status_class(status)
+    STATUS_CLASSES[status.to_sym] || "bg-base-300"
+  end
+
+  helper_method :status_class, :working_hours_for
 end
